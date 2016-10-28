7d55f740d947eff307b8f6bf1862e91455c872bf
NielsenRichard <nielhpuma@outlook.com>
2016-10-27 21:39:10 -0500

    Linqueando Form Salas a Form Principal.

--- ProyectoAtencionCines/src/gui/frmSalas.java
+++ ProyectoAtencionCines/src/gui/frmSalas.java
@@ -54,7 +54,7 @@ public class frmSalas extends JFrame implements ActionListener {
 	 */
 	public frmSalas() {
 		setTitle("Salas");
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setBounds(100, 100, 570, 421);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));

38f6ef10b89b8ec553c325077834b053705111e1
ReyderSolgada <solmisit@gmail.com>
2016-12-07 20:49:59 -0500

    funcion

---
+++ ProyectoAtencionCines/src/clases/Funcion.java
@@ -0,0 +1,48 @@
+package clases;
+
+public class Funcion {
+	//ATRIBUTOS PRIVADOS
+	private int codFuncion, codCine, codPeli;
+	private String fecha, hora;
+	//CONSTRUCTOR
+	public Funcion(int codFuncion, int codCine, int codPeli, String fecha, String hora) {
+		this.codFuncion = codFuncion;
+		this.codCine = codCine;
+		this.codPeli = codPeli;
+		this.fecha = fecha;
+		this.hora = hora;
+	}
+	//METODO SET/GET
+	public int getCodFuncion() {
+		return codFuncion;
+	}
+	public int getCodCine() {
+		return codCine;
+	}
+	public int getCodPeli() {
+		return codPeli;
+	}
+	public String getFecha() {
+		return fecha;
+	}
+	public String getHora() {
+		return hora;
+	}
+	public void setCodFuncion(int codFuncion) {
+		this.codFuncion = codFuncion;
+	}
+	public void setCodCine(int codCine) {
+		this.codCine = codCine;
+	}
+	public void setCodPeli(int codPeli) {
+		this.codPeli = codPeli;
+	}
+	public void setFecha(String fecha) {
+		this.fecha = fecha;
+	}
+	public void setHora(String hora) {
+		this.hora = hora;
+	}
+	
+
+}

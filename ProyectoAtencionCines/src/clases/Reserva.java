52b0b840a1d933cd49e01ad728049a9ddf999293
benyi.rivas@hotmail.com <benyi.rivas@hotmail.com>
2016-12-08 15:37:53 -0500

    clase y array de RESERVA

---
+++ ProyectoAtencionCines/src/clases/Reserva.java
@@ -0,0 +1,83 @@
+package clases;
+
+public class Reserva {
+	//ATRIBUTOS PRIVADOS
+		private int codReserva, codCliente, codEmpleado, codFuncion,estado ;
+		private String fechaReserva, horaReserva;
+	
+	//constructor
+		
+		public Reserva(int codReserva, int codCliente, int codEmpleado, int codFuncion, int estado, String fechaReserva,
+				String horaReserva) {
+			super();
+			this.codReserva = codReserva;
+			this.codCliente = codCliente;
+			this.codEmpleado = codEmpleado;
+			this.codFuncion = codFuncion;
+			this.estado = estado;
+			this.fechaReserva = fechaReserva;
+			this.horaReserva = horaReserva;
+		}
+		
+		//metodos get y set
+		
+		public int getCodReserva() {
+			return codReserva;
+		}
+
+		public void setCodReserva(int codReserva) {
+			this.codReserva = codReserva;
+		}
+
+		public int getCodCliente() {
+			return codCliente;
+		}
+
+		public void setCodCliente(int codCliente) {
+			this.codCliente = codCliente;
+		}
+
+		public int getCodEmpleado() {
+			return codEmpleado;
+		}
+
+		public void setCodEmpleado(int codEmpleado) {
+			this.codEmpleado = codEmpleado;
+		}
+
+		public int getCodFuncion() {
+			return codFuncion;
+		}
+
+		public void setCodFuncion(int codFuncion) {
+			this.codFuncion = codFuncion;
+		}
+
+		public int getEstado() {
+			return estado;
+		}
+
+		public void setEstado(int estado) {
+			this.estado = estado;
+		}
+
+		public String getFechaReserva() {
+			return fechaReserva;
+		}
+
+		public void setFechaReserva(String fechaReserva) {
+			this.fechaReserva = fechaReserva;
+		}
+
+		public String getHoraReserva() {
+			return horaReserva;
+		}
+
+		public void setHoraReserva(String horaReserva) {
+			this.horaReserva = horaReserva;
+		}
+		
+		
+		
+		
+}
---
+++ ProyectoAtencionCines/src/controlador/ArregloReserva.java
@@ -0,0 +1,109 @@
+package controlador;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+
+import clases.Reserva;
+public class ArregloReserva {
+	//ATRIBUTO PRIVADO
+		private ArrayList <Reserva> re;
+		private String archivo;
+		
+	//constructor
+		public ArregloReserva(String archivo) {
+			re= new ArrayList<Reserva>();
+			this.archivo = archivo;
+		}
+		
+		public int tamaño(){
+			return re.size();
+		}
+		public Reserva obtener(int pos){
+			return re.get(pos);
+		}
+		public void adicionar(Reserva x){
+			re.add(x);
+		}
+		public void eliminar(Reserva x){
+			re.remove(x);
+		}
+		
+	//GENERADOR DE CODIGO CORRELATIVO
+		public int codigoCorrelativo() {
+			if (tamaño() == 0)
+				return 10001;
+			else
+			return obtener(tamaño()-1).getCodReserva()+ 1;		
+		}
+		
+	//METODO BUSCAR
+		public Reserva buscar(int codigo) {
+			for (int i=0; i<tamaño(); i++)
+				if (obtener(i).getCodReserva()== codigo)
+					return obtener(i);
+			return null;
+		}	
+		
+		
+	//OPERACIONES PUBLICAS COMPLEMENTARIAS
+		public void grabarFuncion() {
+			try {
+				PrintWriter pw;
+				String linea;
+				Reserva x;
+				pw = new PrintWriter(new FileWriter(archivo));
+				for (int i=0; i<tamaño(); i++) {
+					x = obtener(i);
+					linea = x.getCodReserva() + ";" +
+						    x.getCodCliente()+ ";" +
+							x.getCodEmpleado() + ";" +
+							x.getCodFuncion() + ";" +
+							x.getEstado() + ";" +
+							x.getFechaReserva()+ ";" +
+							x.getHoraReserva();
+					pw.println(linea);
+				}
+				pw.close();
+			}
+			catch (Exception e) {
+			}
+		}
+		
+		
+	//OPERACIONES PUBLICAS COMPLEMENTARIAS	
+		
+		public void cargarReserva() {
+			try {
+				BufferedReader br;
+				String linea, fechareserva, horareserva;
+				String s[];
+				int codReserva, codCliente, codEmpleado, codFuncion, estado;
+				br = new BufferedReader(new FileReader(archivo));
+				while ((linea = br.readLine()) != null) {
+					s = linea.split(";");
+					codReserva = Integer.parseInt(s[0].trim());
+					codCliente = Integer.parseInt(s[1].trim());
+					codEmpleado = Integer.parseInt(s[2].trim());
+					codFuncion = Integer.parseInt(s[3].trim());
+					estado = Integer.parseInt(s[4].trim());
+					fechareserva = s[5].trim();
+					horareserva = s[6].trim();
+					adicionar(new Reserva( codReserva,  codCliente,  codEmpleado,  codFuncion, estado, fechareserva , horareserva));
+				}
+				br.close();
+			}
+			catch (Exception e) {
+			}
+		}		
+		public boolean existeArchivo() {
+			File f = new File(archivo);
+			return f.exists();
+		}	
+		
+	
+		
+}

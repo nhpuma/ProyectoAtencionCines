afa2dd38a769ac04d997d38955f9a6960f57bbbb
NielsenRichard <nielhpuma@outlook.com>
2016-12-15 01:39:48 -0500

    Implementando el frm Control de Pago

--- ProyectoAtencionCines/src/gui/frmControlPago.java
+++ ProyectoAtencionCines/src/gui/frmControlPago.java
@@ -7,24 +7,42 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 import javax.swing.JButton;
 import javax.swing.JTextArea;
 import java.awt.event.ActionListener;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.awt.event.ActionEvent;
 import javax.swing.border.TitledBorder;
 import javax.swing.table.DefaultTableModel;
 
 import Vistas.Dialogo;
+import clases.Cine;
+import clases.Cliente;
+import clases.DetalleReserva;
 import clases.Empleado;
+import clases.Funcion;
+import clases.Pelicula;
 import clases.Reserva;
+import controlador.ArregloButaca;
+import controlador.ArregloCine;
+import controlador.ArregloCliente;
+import controlador.ArregloDetalleReserva;
+import controlador.ArregloFuncion;
+import controlador.ArregloPelicula;
 import controlador.ArregloReserva;
+import controlador.ArregloSala;
 
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.ImageIcon;
 import javax.swing.UIManager;
 import java.awt.Color;
+import javax.swing.SwingConstants;
 
 public class frmControlPago extends JFrame implements ActionListener {
 
@@ -95,7 +113,7 @@ public class frmControlPago extends JFrame implements ActionListener {
 		
 		panel = new JPanel();
 		panel.setBorder(new TitledBorder(UIManager.getBorder("TitledBorder.border"), "Detalle Entrada", TitledBorder.LEADING, TitledBorder.TOP, null, new Color(0, 0, 0)));
-		panel.setBounds(10, 213, 499, 245);
+		panel.setBounds(10, 213, 499, 231);
 		contentPane.add(panel);
 		panel.setLayout(null);
 		
@@ -119,16 +137,19 @@ public class frmControlPago extends JFrame implements ActionListener {
 		panel.add(lblTotal);
 		
 		txtsubTotal = new JTextField();
+		txtsubTotal.setHorizontalAlignment(SwingConstants.RIGHT);
 		txtsubTotal.setBounds(403, 143, 86, 20);
 		panel.add(txtsubTotal);
 		txtsubTotal.setColumns(10);
 		
 		txtIGV = new JTextField();
+		txtIGV.setHorizontalAlignment(SwingConstants.RIGHT);
 		txtIGV.setColumns(10);
 		txtIGV.setBounds(403, 168, 86, 20);
 		panel.add(txtIGV);
 		
 		txtTotal = new JTextField();
+		txtTotal.setHorizontalAlignment(SwingConstants.RIGHT);
 		txtTotal.setColumns(10);
 		txtTotal.setBounds(403, 193, 86, 20);
 		panel.add(txtTotal);
@@ -145,15 +166,33 @@ public class frmControlPago extends JFrame implements ActionListener {
 		contentPane.add(btnNewButton);
 		
 		m = new DefaultTableModel();
+		m.addColumn("Butaca");
 		m.addColumn("Cantidad");
 		m.addColumn("Tipo Entrada");
 		m.addColumn("Precio Unit");
 		m.addColumn("Importe");
 		table.setModel(m);
+		
+		btnPagar = new JButton("Pagar");
+		btnPagar.addActionListener(this);
+		btnPagar.setBounds(321, 469, 89, 23);
+		contentPane.add(btnPagar);
 
 
 	}
+	//Instancias a las ArrayList
+	ArregloFuncion af = new ArregloFuncion("Funcion.txt");
+	ArregloCine ac = new ArregloCine("cine.txt");
+	ArregloCliente aCli = new ArregloCliente("Cliente.txt");
+	ArregloDetalleReserva aDR = new ArregloDetalleReserva();
+	ArregloPelicula ap = new ArregloPelicula("Pelicula.txt");
+	ArregloSala as = new ArregloSala("Sala.txt");
+	ArregloButaca ab = new ArregloButaca("Butacas.txt");
+	
 	public void actionPerformed(ActionEvent arg0) {
+		if (arg0.getSource() == btnPagar) {
+			actionPerformedBtnPagar(arg0);
+		}
 		if (arg0.getSource() == btnNewButton) {
 			actionPerformedBtnNewButton(arg0);
 		}
@@ -165,6 +204,7 @@ public class frmControlPago extends JFrame implements ActionListener {
 		}
 	}
 	ArregloReserva ar = new ArregloReserva("Reserva.txt");
+	private JButton btnPagar;
 	protected void actionPerformedBtnNewButton(ActionEvent arg0) {
 		Dialogo <Reserva> r = new Dialogo <> (ar.getRe());
 		r.mostrar();
@@ -174,17 +214,59 @@ public class frmControlPago extends JFrame implements ActionListener {
 			cod = r.getSelect().substring(0,cod.indexOf("-"));
 			txtcodReserva.setText(cod);
 		}
-		imprimir();
-		imprimir("Nombre del Cine	:");
-		imprimir("Codigo Empleado	:");
-		imprimir("Codigo Cliente		:");
-		imprimir("Nombre Cliente		:");
-		imprimir("Codigo Sala		:");
-		imprimir("Titulo Pelicula		:");
-		imprimir("Butacas Reservadas	:");
+//		imprimir();
+//		imprimir("Nombre del Cine	:");
+//		imprimir("Codigo Empleado	:");
+//		imprimir("Codigo Cliente		:");
+//		imprimir("Nombre Cliente		:");
+//		imprimir("Codigo Sala		:");
+//		imprimir("Titulo Pelicula		:");
+//		imprimir("Butacas Reservadas	:");
 	}
 
-	protected void actionPerformedBtnImprimir(ActionEvent arg0) {	
+	protected void actionPerformedBtnImprimir(ActionEvent arg0) {
+		//Buscando la reserva
+		String codR = txtcodReserva.getText().trim();
+		if(!codR.equals("")){
+			Reserva r = ar.buscar(Integer.parseInt(codR));
+			if(r != null){
+				int codFuncion = r.getCodiFuncion();
+				//Buscando la función
+				Funcion f = af.buscar(codFuncion);
+				if(f != null){
+					int codCine = f.getCodCine();
+					//Busncado el Cine
+					Cine c = ac.buscar(codCine);
+					if(c != null){
+						txtS.setText("");
+						imprimir("Nombre Cine\t\t: "+c.getNombre());
+						imprimir("Cod. Empleado\t\t: "+r.getCodEmpleado());
+						imprimir("Cod. Cliente\t\t: "+r.getCodCliente());
+						//Buscando Cliente
+						Cliente cli = aCli.buscar(r.getCodEmpleado());
+						imprimir("Nombre Cliente\t\t: "+cli.getNombre());
+						imprimir("Apellidos Cliente\t: "+cli.getApepat()+" "+cli.getApemat());
+						imprimir("Cod.Sala\t\t: "+f.getCodSala());
+						//Buscando Pelìcula
+						int codPeli = f.getCodCine();
+						Pelicula p = ap.buscar(codPeli); 
+						imprimir("Película\t\t: "+p.getTitDistribucion());
+						imprimir("Butacas Reservadas\t: ");
+						imprimir("Nº de Entradas\t: ");
+						imprimir("Precio\t\t: ");
+						imprimir("Importe Parcial\t: ");
+						
+						listarDetalleReserva(Integer.parseInt(codR));
+						
+					}//Fin if Cine
+				}//Fin if Función
+			}else{
+				mensaje("No se encuentra la reserva");
+			}
+		}else{
+			mensaje("Ingrese el Código de Reserva");
+			txtcodReserva.requestFocus();
+		}
 	}
 	protected void actionPerformedBtnSalir(ActionEvent arg0) {
 		dispose();
@@ -196,6 +278,91 @@ public class frmControlPago extends JFrame implements ActionListener {
 	void imprimir(String s) {
 		txtS.append(s + "\n");
 	}
-
+	void mensaje(String s){
+		JOptionPane.showMessageDialog(this, s);
+	}
 	
+	void listarDetalleReserva(int codRes){
+		m.setRowCount(0);
+		DetalleReserva dr;
+		double sum = 0;
+		for (int i = 0; i < aDR.tamanho(); i++) {
+			dr = aDR.obtener(i);
+			if(dr.getCodReserva() == codRes){
+				Object fila [] = {
+					dr.getCodButaca(),
+					dr.getCantidad(),
+					dr.detalleTipoEntrada(),
+					dr.getPrecio(),
+					dr.getCantidad()*dr.getPrecio()					
+				};
+				m.addRow(fila);
+				sum = sum + (dr.getCantidad()*dr.getPrecio());
+			}//Fin IF
+		}//Fin FOR
+		
+		txtsubTotal.setText(""+redondear(sum, 2));
+		//Cálculo del IGV
+		double igv = sum*0.18;
+		txtIGV.setText(""+redondear(igv, 2));		
+		//Cálculo del subtotal + igv
+		double total = sum+igv;
+		txtTotal.setText(""+redondear(total, 2));
+	}
+	
+	BigDecimal redondear(double numero, int digitos){
+		String val = numero+"";
+		BigDecimal big = new BigDecimal(val);
+		big = big.setScale(digitos, RoundingMode.HALF_UP);
+		return big;
+	}
+	//Función que compara fechas
+	boolean compararFechaFuncion(String fechaFuncion, String fechaActual){
+		boolean fun = false;
+		try {
+			SimpleDateFormat formateador = new SimpleDateFormat("dd/MM/yyyy");
+			Date fechaDateF = formateador.parse(fechaFuncion);
+			Date fechaDateA = formateador.parse(fechaActual);
+			
+			if(fechaDateF.before(fechaDateA)){
+				//mensaje("Ya caducó su Reserva");
+				fun = false;
+			}else{
+				fun = true;
+			}
+		}
+		catch (Exception e) {
+			fun = false;
+			System.out.println("Error!! "+e);
+		}		
+		return fun;
+	}
+	protected void actionPerformedBtnPagar(ActionEvent arg0) {
+		//Verificando si está en la fecha correcta
+		String codRes = txtcodReserva.getText().trim();
+		if(codRes.equals("")){
+			mensaje("Ingrese el Código de la Reserva");
+		}else{
+			Reserva r = ar.buscar(Integer.parseInt(codRes));
+			if(r != null){
+				int codFuncion = r.getCodiFuncion();
+				//Buscando la función
+				Funcion f = af.buscar(codFuncion);
+				String fechaFuncion = f.getFecha();
+				Date d = new Date();
+				SimpleDateFormat formateador = new SimpleDateFormat("dd/MM/yyyy");
+				String fechaActual = formateador.format(d);
+				if(compararFechaFuncion(fechaFuncion, fechaActual)){
+					mensaje("Pago de la reserva procesada");
+					r.setEstado(1);
+					ar.grabarReserva();
+				}else{
+					mensaje("Se Reserva Caducó");
+				}
+			}else{
+				mensaje("No se encontró la Reserva");
+			}
+		}
+		
+	}
 }
